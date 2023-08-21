b6961870800e880c64a908b0cb2b948f10c1c902
Marcelo Ramos Soares <marceloxramos3d@gmail.com>
2023-08-21 17:00:24 -0300

    Hello World Controller created for testing

---
+++ BookApplication/Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookApplication.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "Default Hello World view";
+        }
+
+        // GET: /HelloWorld/Welcome/
+        public string Welcome() {
+            return "This is the Welcome method... ";
+        } 
+    }
+}
