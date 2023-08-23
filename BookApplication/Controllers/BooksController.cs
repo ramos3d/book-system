eac6531d7e6484b2f6d0ce6a6b476876f8e1b106
Marcelo Ramos Soares <marceloxramos3d@gmail.com>
2023-08-23 12:35:57 -0300

    Implemented Update & Delete

--- BookApplication/Controllers/BooksController.cs
+++ BookApplication/Controllers/BooksController.cs
@@ -2,6 +2,8 @@
 using BookApplication.Models;
 using BookApplication.Models.Domain;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
 
 namespace BookApplication.Controllers
 {
@@ -13,6 +15,12 @@ namespace BookApplication.Controllers
         {
 			this.bookDbContext = bookDbContext;
 		}
+        [HttpGet]
+        public async Task<IActionResult> Index() 
+        { 
+            var books_list = await bookDbContext.Books.ToListAsync();
+            return View(books_list);
+        }
 
         [HttpGet]
         public IActionResult Add()
@@ -33,7 +41,59 @@ namespace BookApplication.Controllers
 
 			await bookDbContext.AddAsync(book);
             await bookDbContext.SaveChangesAsync();
-            return RedirectToAction("Add");
+            return RedirectToAction("Index");
 		}
+
+        [HttpGet] 
+        public async Task<IActionResult> View(Guid id) 
+        {
+            var book =await bookDbContext.Books.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (book != null)
+            {
+                var viewModel = new UpdateBookViewModel()
+                {
+                    Id = book.Id,
+                    Title = book.Title,
+                    YearPublished = book.YearPublished,
+                    Author = book.Author
+                };
+                return await Task.Run(() => View("View", viewModel));
+                
+            }
+                return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(UpdateBookViewModel model)
+        {
+            var book = await bookDbContext.Books.FindAsync(model.Id);
+            if (book != null)
+            {
+                book.Author = model.Author;
+                book.Title = model.Title;   
+                book.YearPublished = model.YearPublished;
+                await bookDbContext.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(UpdateBookViewModel model)
+        { 
+            var book = await bookDbContext.Books.FindAsync(model.Id);
+            if (book != null)
+            {
+                bookDbContext.Books.Remove(book);
+                await bookDbContext.SaveChangesAsync();
+                TempData["success"] = "Book "+ model.Title +" has been deleted sucessfully!";
+                return RedirectToAction("Index");
+            }
+            TempData["error"] = "Something went wrong!";
+            return RedirectToAction("Index");
+
+        }
     }
 }
--- BookApplication/wwwroot/js/site.js
+++ BookApplication/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+function confirmDelete(bookId) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            // Trigger form submission
+            document.getElementById('deleteForm').submit();
+        }
+    });
+}
