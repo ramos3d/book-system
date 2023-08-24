ebe29c0324faf6cbb95085c93ebaf7310ab0cab8
Marcelo Ramos Soares <marceloxramos3d@gmail.com>
2023-08-24 11:45:01 -0300

    Added the Cover column at Books table

---
+++ BookApplication/Migrations/20230824142404_AddCoverColumnToBooksTable.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookApplication.Migrations
+{
+	/// <inheritdoc />
+	public partial class AddCoverColumnToBooksTable : Migration
+	{
+		/// <inheritdoc />
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.AddColumn<string>(
+			name: "Cover",
+			table: "Books",
+			type: "nvarchar(max)",
+			nullable: true);
+
+		}
+
+		/// <inheritdoc />
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
+
+		}
+	}
+}
