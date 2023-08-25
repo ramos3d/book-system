ebf2a1239e76b607c17eb8673c0edbdccb4c57de
Marcelo Ramos Soares <marceloxramos3d@gmail.com>
2023-08-25 14:36:11 -0300

    Created AddUserViewModel.cs for user registration and Users.cs for the Users table

---
+++ BookApplication/Models/AddUserViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BookApplication.Models
+{
+    public class AddUserViewModel
+    {
+		public string name { get; set; }
+		public string email { get; set; }
+		public string password { get; set; }
+	}
+}
---
+++ BookApplication/Models/Domain/Users.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace BookApplication.Models.Domain
+{
+    public class Users
+    {
+		
+		public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string Password { get; set; }
+    }
+}
