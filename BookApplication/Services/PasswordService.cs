ae820e047d80e960fa05fbe91eaeb1af5a76887d
Marcelo Ramos Soares <marceloxramos3d@gmail.com>
2023-08-25 14:43:42 -0300

    Created a new service for password hashing

---
+++ BookApplication/Services/PasswordService.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace BookApplication.Services
+{
+	public class PasswordService
+	{
+		public string HashPassword(string password)
+		{
+			using var sha256 = SHA256.Create();
+			byte[] bytes = Encoding.UTF8.GetBytes(password);
+			byte[] hash = sha256.ComputeHash(bytes);
+
+			// Convert the hash to a hexadecimal string
+			StringBuilder builder = new StringBuilder();
+			foreach (byte b in hash)
+			{
+				builder.Append(b.ToString("x2"));
+			}
+
+			return builder.ToString();
+		}
+	}
+}
