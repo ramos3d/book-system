9016166f23f597c7253bfa411fc7e071d59f5f09
Marcelo Ramos Soares <marceloxramos3d@gmail.com>
2023-08-25 14:33:19 -0300

    Created new migration files for creating Users and UserHistory tables

---
+++ BookApplication/Migrations/20230825165738_CreateUsersTable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookApplication.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateUsersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+					Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+					Name = table.Column<string>(nullable: true),
+					Email = table.Column<string>(nullable: true),
+					Password = table.Column<string>(nullable: true)
+				},
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ BookApplication/Migrations/20230825170416_CreateUserHistoryTable.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookApplication.Migrations
+{
+	/// <inheritdoc />
+	public partial class CreateUserHistoryTable : Migration
+	{
+		/// <inheritdoc />
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.CreateTable(
+			name: "User_History",
+			columns: table => new
+			{
+				Id = table.Column<Guid>(nullable: false),
+				Users_Id = table.Column<Guid>(nullable: false),
+				To_Buy = table.Column<string>(nullable: true),
+				Owned_Books = table.Column<string>(nullable: true)
+			},
+			constraints: table =>
+			{
+				table.PrimaryKey("PK_User_History", x => x.Id);
+				table.ForeignKey(
+					name: "FK_User_History_Users_Users_Id",
+					column: x => x.Users_Id,
+					principalTable: "Users",
+					principalColumn: "Id",
+					onDelete: ReferentialAction.Cascade);
+			});
+	
+
+		}
+
+		/// <inheritdoc />
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.DropTable(
+				name: "User_History");
+
+		}
+	}
+}
